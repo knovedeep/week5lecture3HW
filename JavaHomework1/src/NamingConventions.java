9f90cbdb19d1596b2721d67475967bf68a780519
knovedeep <novedeepkr@gmail.com>
2023-01-15 13:43:42 -0800

    added files

---
+++ JavaHomework1/src/NamingConventions.java
@@ -0,0 +1,25 @@
+
+public class NamingConventions {
+
+	public static void main(String[] args) {
+		int currentScoreOfGame= notanumber;
+		int workingVariable= 10;
+		int workingVariable2=15;
+		boolean answer=;
+		System.out.println(answer);
+		boolean answer2= notaBooleanValue;
+		String value1="a string";
+		byte bigvalue=168;
+		if (workingVariable < workingVariable2) {
+			System.out.println("These are working variables");
+			int value2=10;
+			System.out.println(value2);
+			String value3=;
+			System.out.println(value3);
+		}
+	System.out.println(value2);
+	System.out.println(value3);
+	}
+	
+
+}
